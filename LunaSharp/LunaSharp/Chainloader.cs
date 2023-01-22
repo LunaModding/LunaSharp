a22bcafbd1209a0dc406cd751cc28bdb31033640
Millzy <millzyyt08@gmail.com>
2023-01-22 09:30:51 +0000

    chainloader singleton

---
+++ LunaSharp/LunaSharp/Chainloader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunaSharp
+{
+    public class Chainloader
+    {
+        private static readonly Chainloader _instance = new Chainloader();
+
+        static Chainloader()
+        {
+        }
+
+        private Chainloader()
+        {
+        }
+
+        public static Chainloader Instance
+        {
+            get => _instance;
+        }
+
+        public static string AddonsDirectoryPath
+        {
+            get => Path.Combine(Environment.CurrentDirectory, "Luna", "Addons");
+        }
+    }
+}
